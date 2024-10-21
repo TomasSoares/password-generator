5e5952d996c216d4fcaf8eac04198a441df49465
Tomás Soares <soarestomas2003@gmail.com>
2024-10-21 18:13:40 +0100

    Added the possibility to choose the length of the passowrd

--- pass-gen.py
+++ pass-gen.py
@@ -2,9 +2,9 @@ import random
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*().,?0123456789'
 password = ''
-password_length = 16
+password_length = int(input("Enter the length of password: "))
 
-for j in range(password_length):
+for i in range(password_length):
     password += random.choice(chars)
 
 print("Password:\n",password)
