39d341ef1463f0a6e2d2155de6e4ef90fa945a1c
Tomás Soares <soarestomas2003@gmail.com>
2024-10-24 18:50:13 +0100

    Cleared lines

--- pass-gen.py
+++ pass-gen.py
@@ -11,7 +11,6 @@ import pyperclip
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*().,?0123456789'
 password = ''
 
-
 print("="*40)
 print("        PASSWORD GENERATOR        ")
 print("="*40)
@@ -26,12 +25,10 @@ while True:
     except ValueError:
         print("Please enter a valid number")
 
-
 for i in range(password_length):
     password += random.choice(chars)
 
 print("Password:\n", password)
-
 
 def get_yes_or_no_input(msg):
     while True:
@@ -41,16 +38,13 @@ def get_yes_or_no_input(msg):
         else:
             print("Please enter 'Y' for Yes or 'N' for No.")
 
-
 save_password = get_yes_or_no_input("\nDo you want to save the password to a file?\n(Y)es or (N)o: ")
 copy_to_clipboard = get_yes_or_no_input("\nDo you want to copy the password to clipboard?\n(Y)es or (N)o: ")
-
 
 if save_password == "y":
     with open("password.txt", "w") as file:
         file.write(password)
     print("\nPassword saved to file password.txt")
-
 
 if copy_to_clipboard == "y":
     pyperclip.copy(password)
