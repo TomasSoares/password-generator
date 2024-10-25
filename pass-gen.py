7c41565b1d967aeb7e79d1fdd633bf897905698c
Tomás Soares <soarestomas2003@gmail.com>
2024-10-25 17:50:36 +0100

    Possibility to create multiple passwords

--- pass-gen.py
+++ pass-gen.py
@@ -9,26 +9,11 @@ import random
 import pyperclip
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*().,?0123456789'
-password = ''
+password = ""
 
 print("="*40)
 print("        PASSWORD GENERATOR        ")
 print("="*40)
-
-while True:
-    try:
-        password_length = int(input("\nEnter the length of password: "))
-        if password_length > 0:
-            break
-        else:
-            print("Password length should be greater than 0")
-    except ValueError:
-        print("Please enter a valid number")
-
-for i in range(password_length):
-    password += random.choice(chars)
-
-print("Password:\n", password)
 
 def get_yes_or_no_input(msg):
     while True:
@@ -38,16 +23,36 @@ def get_yes_or_no_input(msg):
         else:
             print("Please enter 'Y' for Yes or 'N' for No.")
 
-save_password = get_yes_or_no_input("\nDo you want to save the password to a file?\n(Y)es or (N)o: ")
-copy_to_clipboard = get_yes_or_no_input("\nDo you want to copy the password to clipboard?\n(Y)es or (N)o: ")
+while True:
+    while True:
+        try:
+            password_length = int(input("\nEnter the length of password: "))
+            if password_length > 0:
+                break
+            else:
+                print("Password length should be greater than 0")
+        except ValueError:
+            print("Please enter a valid number")
 
-if save_password == "y":
-    with open("password.txt", "w") as file:
-        file.write(password)
-    print("\nPassword saved to file password.txt")
+    for i in range(password_length):
+        password += random.choice(chars)
 
-if copy_to_clipboard == "y":
-    pyperclip.copy(password)
-    print("\nPassword copied to clipboard")
+    print("Password:\n", password)
 
-input("\nPress Enter to exit...")
+    save_password = get_yes_or_no_input("\nDo you want to save the password to a file?\n(Y)es or (N)o: ")
+    if save_password == "y":
+        with open("password.txt", "a") as file:
+            file.write(password + "\n")
+        print("\nPassword saved to file password.txt")
+
+    copy_to_clipboard = get_yes_or_no_input("\nDo you want to copy the password to clipboard?\n(Y)es or (N)o: ")
+    if copy_to_clipboard == "y":
+        pyperclip.copy(password)
+        print("\nPassword copied to clipboard")
+
+    another_pass = get_yes_or_no_input("\nDo you want to generate another password?\n(Y)es or (N)o: ")
+    if another_pass == "n":
+        print("\nThank you for using Password Generator!")
+        print("\nGoodbye!")
+        input("\nPress Enter to exit...")
+        break  
